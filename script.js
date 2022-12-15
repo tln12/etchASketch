06d2773e27c9d09d629e64400205c5a8d623e10a
nhopo <thanh.le.nguyen@rwth-aachen.de>
2022-12-15 20:43:48 +0100

    Add event listener for button

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ for (let i = 0; i < 16; i++){
     div.setAttribute('class', 'square');
     container.appendChild(div);
 }
+
+const btn = document.querySelector('button');
+let num;
+btn.addEventListener('click', () => {
+
+    do {
+        num = Number(prompt("Type in the number of squares per side of the grid (max. 100)."));
+    } while (isNaN(num) || num > 100);
+
+    return num;
+})
