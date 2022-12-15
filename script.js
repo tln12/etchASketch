654da4ed58270aba11e67e849b7b34eb74e364b2
nhopo <thanh.le.nguyen@rwth-aachen.de>
2022-12-15 18:43:43 +0100

    Add 16 squares into div

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hi");
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++){
+    const div = document.createElement('div');
+    div.setAttribute('class', 'square');
+    container.appendChild(div);
+}
