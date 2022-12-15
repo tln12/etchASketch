9616ffb4a46f91dd6c608398ad58c4b133246532
nhopo <thanh.le.nguyen@rwth-aachen.de>
2022-12-15 20:56:15 +0100

    Automate square width calculation

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const btn = document.querySelector('button');
 let num;
 btn.addEventListener('click', () => {
@@ -8,17 +6,20 @@ btn.addEventListener('click', () => {
         num = Number(prompt("Type in the number of squares per side of the grid (max. 100)."));
     } while (isNaN(num) || num > 100);
 
-    return num;
+    createGrid(num);
 })
 
 function createGrid(squaresPerRow){
     const container = document.querySelector('.container');
+    let squareWidth;
     squares = squaresPerRow ** 2;
     for (let i = 0; i < squares; i++){
         const div = document.createElement('div');
         div.setAttribute('class', 'square');
+        squareWidth = 1/squaresPerRow * 100;
+        div.style.width = `${squareWidth}%`
         container.appendChild(div);
     }
 }
 
-createGrid(4);
+createGrid(10);
